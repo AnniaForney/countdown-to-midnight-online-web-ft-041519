d1eb2c5fb65934c85db483be08c9a3e6bbd6573f
Annia Forney <anniaforney@gmail.com>
2019-04-19 19:35:26 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
+x = 4 
+while x > 0 
+  puts "#{number} SECOND(S)!"
+  x += 0
+end 
 
 def countdown
 end
+
+def countdown_with_sleep 
+  x = 6
+  puts "#{number} SECOND(S)!"
+  x += 0
+end 
